729d23968c7e0dcafa6a889ac2fb606634d51fa3
Matías <dmcabrera90@gmail.com>
2022-04-28 19:45:30 -0300

    master

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
 
+//Setear servidor
+app.use(express.static(path.join(__dirname, "build")));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//RUTAS
 app.post(
   "/api/auth/signup",
   [verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted],
@@ -18,28 +24,27 @@ app.post(
   controller.signup
 );
 app.post("/api/auth/signin", controller.signin);
-
-var corsOptions = {
-  origin: "*",
-};
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 const PORT = process.env.PORT || 8080;
+
+
+//Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Hola pibe, estoy corriendo en: ${PORT}.`);
 });
 
-const db = require("./models");
 
+
+////Inicializar Base de datos
+const db = require("./models");
 const Role = db.role;
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and Resync Db");
   initial();
 });
+
 
 function initial() {
   Role.create({
